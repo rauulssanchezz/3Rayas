81b67dac9251f1094451472065971ddff15d8faa
EAG <rauulssanchezz@gmail.com>
2023-11-09 17:47:39 +0100

    ertgyuio

---
+++ app/src/main/java/com/example/a3rayas/Ej2.kt
@@ -0,0 +1,11 @@
+package com.example.a3rayas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ej2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ej2)
+    }
+}
